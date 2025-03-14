6a4b74619940d59ae6b46f671ade63a58bab6ac5
Federico Cabanilla <federico.cabanilla@gmail.com>
2025-03-14 16:23:06 -0300

    Eliminar archivos de configuración de ESLint y actualizar la descripción del proyecto en package.json. Agregar nuevas configuraciones de ESLint y crear un archivo de fixtures para pruebas con Playwright.

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+// eslint.config.js
+export default {
+  root: true,
+  ignorePatterns: ["node_modules/", "dist/"],
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+};
--- tests/cinesa/navbar.spec.ts
+++ tests/cinesa/navbar.spec.ts
@@ -1,6 +1,5 @@
-import { test } from '@playwright/test';
-import { CookieBanner } from '../../pageObjectsManagers/cinesa/cookieBanner';
-import { Navbar } from '../../pageObjectsManagers/cinesa/navbar';
+// tests/cinesa/navbar.spec.ts
+import { test } from '../../fixtures/cinesa/playwright.fixtures';
 import {
   assertNavbarElementsVisible,
   assertHomeUrl,
@@ -10,43 +9,32 @@ import {
 import { baseUrl, internalNavItems, externalNavItem } from './navbar.data';
 
 test.describe('Cinesa Navbar Tests', () => {
-  test.beforeEach(async ({ page }) => {
-    // Navegar al home y aceptar cookies
-    const navbar = new Navbar(page);
-    const cookieBanner = new CookieBanner(page);
-
+  test.beforeEach(async ({ navbar, cookieBanner }) => {
     await navbar.navigateToHome();
     await cookieBanner.acceptCookies();
   });
 
-  test('should display all navbar elements', async ({ page }) => {
-    const navbar = new Navbar(page);
+  test('should display all navbar elements', async ({ navbar, page }) => {
     await assertNavbarElementsVisible(page, navbar.selectors);
   });
 
-  test('should click logo and stay on home', async ({ page }) => {
-    const navbar = new Navbar(page);
+  test('should click logo and stay on home', async ({ navbar, page }) => {
     await navbar.clickLogo();
     await assertHomeUrl(page, baseUrl);
   });
 
   test('should click each navbar element and navigate accordingly', async ({
+    navbar,
     page,
   }) => {
-    const navbar = new Navbar(page);
-
-    // Validación para enlaces internos
     for (const item of internalNavItems) {
       await assertNavClick(
         page,
         navbar.selectors[item.selectorKey],
         item.expectedUrl
       );
-      // Navegar de vuelta a la home para el siguiente check
       await navbar.navigateToHome();
     }
-
-    // Validación para enlace externo ('bonos')
     await assertExternalNavClick(
       page,
       navbar.selectors[externalNavItem.selectorKey],
