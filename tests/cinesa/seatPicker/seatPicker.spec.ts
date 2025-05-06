ab4f6dc70eb54506c1a4edf6dc4afc6f6751dd31
matiasslpknt <mmanzanelli069@alumnos.iua.edu.ar>
2025-05-06 11:14:30 -0300

    feat: Add Santander cinema selection method and update tests to use it

--- tests/cinesa/seatPicker/seatPicker.spec.ts
+++ tests/cinesa/seatPicker/seatPicker.spec.ts
@@ -5,42 +5,7 @@ test.describe('Seat Picker', () => {
     await page.goto('https://www.cinesa.es/');
   });
 
-  // test('Simulate a Full Purchase', async ({
-  //   page,
-  //   navbar,
-  //   cinema,
-  //   cinemaDetail,
-  //   cookieBanner,
-  //   seatPicker,
-  //   ticketPicker,
-  //   loginPage,
-  //   barPage,
-  //   purchaseSummary,
-  //   paymentPage
-  // }) => {
-  //   await cookieBanner.acceptCookies();
-  //   await navbar.navigateToCinemas();
-  //   const selectedCinema = await cinema.selectOasizCinema();
-  //   console.log(`Selected cinema: ${selectedCinema}`);
-
-  //   const { film, showtime } = await cinemaDetail.selectRandomFilmAndShowtime();
-  //   console.log(`Selected film: ${film} at showtime: ${showtime}`);
-
-  //   const selectedSeat = await seatPicker.selectLastAvailableSeat();
-  //   console.log(
-  //     `Selected seat: Row ${selectedSeat.row}, Seat ${selectedSeat.seatNumber}`
-  //   );
-
-  //   await seatPicker.confirmSeats();
-  //   await loginPage.clickContinueAsGuest();
-  //   await ticketPicker.selectTicket();
-  //   await barPage.skipBar();
-  //   await purchaseSummary.acceptAndContinue();
-  //   await paymentPage.completePayment();
-  // });
-
-  test('Simulate a Full Purchase with multiple seats', async ({
-    page,
+  test('Simulate a Full Purchase', async ({
     navbar,
     cinema,
     cinemaDetail,
@@ -54,7 +19,40 @@ test.describe('Seat Picker', () => {
   }) => {
     await cookieBanner.acceptCookies();
     await navbar.navigateToCinemas();
-    const selectedCinema = await cinema.selectOasizCinema();
+    const selectedCinema = await cinema.selectSantanderCinema();
+    console.log(`Selected cinema: ${selectedCinema}`);
+
+    const { film, showtime } = await cinemaDetail.selectRandomFilmAndShowtime();
+    console.log(`Selected film: ${film} at showtime: ${showtime}`);
+
+    const selectedSeat = await seatPicker.selectLastAvailableSeat();
+    console.log(
+      `Selected seat: Row ${selectedSeat.row}, Seat ${selectedSeat.seatNumber}`
+    );
+
+    await seatPicker.confirmSeats();
+    await loginPage.clickContinueAsGuest();
+    await ticketPicker.selectTicket();
+    await barPage.skipBar();
+    await purchaseSummary.acceptAndContinue();
+    await paymentPage.completePayment();
+  });
+
+  test('Simulate a Full Purchase with multiple seats', async ({
+    navbar,
+    cinema,
+    cinemaDetail,
+    cookieBanner,
+    seatPicker,
+    ticketPicker,
+    loginPage,
+    barPage,
+    purchaseSummary,
+    paymentPage
+  }) => {
+    await cookieBanner.acceptCookies();
+    await navbar.navigateToCinemas();
+    const selectedCinema = await cinema.selectSantanderCinema();
     console.log(`Selected cinema: ${selectedCinema}`);
 
     const { film, showtime } = await cinemaDetail.selectRandomFilmAndShowtime();
