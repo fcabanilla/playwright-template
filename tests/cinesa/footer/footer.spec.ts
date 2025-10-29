badc0a7ace61b58ddcb8257f5b2fb59a4a50fd3d
matiasslpknt1 <matiasslpknt08@gmail.com>
2025-10-29 16:51:30 -0300

    feat: apply ADR compliance to footer and experiences tests

--- tests/cinesa/footer/footer.spec.ts
+++ tests/cinesa/footer/footer.spec.ts
@@ -10,40 +10,51 @@ import {
 } from './footer.assertions';
 
 test.describe('Cinesa Footer Tests', () => {
-  test.beforeEach(async ({ page, footer, cookieBanner }) => {
+  test.beforeEach(async ({ footer, cookieBanner, promotionalModal }) => {
     await footer.navigateToHome();
-
-    const acceptButton = page.locator('#onetrust-accept-btn-handler');
-    if (await acceptButton.isVisible()) {
-      await cookieBanner.acceptAllCookies();
-    }
+    await cookieBanner.acceptAllCookies();
+    await promotionalModal.closeModalIfVisible();
   });
 
-  test('@smoke @critical @footer @cinesa should display essential footer navigation elements', async ({ page, footer }) => {
-    await assertFooterCoreElementsVisible(page, footer.selectors);
+  test('should display essential footer navigation elements', 
+    { tag: ['@smoke', '@critical', '@footer', '@cinesa'] },
+    async ({ webActions, footer }) => {
+    await assertFooterCoreElementsVisible(webActions.getPage(), footer.selectors);
   });
 
-  test('@regression @footer @cinesa should display all footer elements comprehensively', async ({ page, footer }) => {
-    await assertFooterElementsVisible(page, footer.selectors);
+  test('should display all footer elements comprehensively', 
+    { tag: ['@regression', '@footer', '@cinesa'] },
+    async ({ webActions, footer }) => {
+    await assertFooterElementsVisible(webActions.getPage(), footer.selectors);
   });
 
-  test('@smoke @footer @navigation @cinesa should navigate to blog and validate URL', async ({ page, footer }) => {
-    await assertNavigateToBlog(page, footer.selectors);
+  test('should navigate to blog and validate URL', 
+    { tag: ['@smoke', '@footer', '@navigation', '@cinesa'] },
+    async ({ webActions, footer }) => {
+    await assertNavigateToBlog(webActions.getPage(), footer.selectors);
   });
 
-  test('@smoke @footer @company @cinesa should navigate to company pages', async ({ page, footer }) => {
-    await assertFooterCompanyNavigation(page, footer.selectors);
+  test('should navigate to company pages', 
+    { tag: ['@smoke', '@footer', '@company', '@cinesa'] },
+    async ({ webActions, footer }) => {
+    await assertFooterCompanyNavigation(webActions.getPage(), footer.selectors);
   });
 
-  test('@fast @footer @social @cinesa should display all social media links', async ({ page, footer }) => {
-    await assertFooterSocialMediaLinks(page, footer.selectors);
+  test('should display all social media links', 
+    { tag: ['@fast', '@footer', '@social', '@cinesa'] },
+    async ({ webActions, footer }) => {
+    await assertFooterSocialMediaLinks(webActions.getPage(), footer.selectors);
   });
 
-  test('@medium @footer @legal @cinesa should display legal documentation links', async ({ page, footer }) => {
-    await assertFooterLegalLinks(page, footer.selectors);
+  test('should display legal documentation links', 
+    { tag: ['@medium', '@footer', '@legal', '@cinesa'] },
+    async ({ webActions, footer }) => {
+    await assertFooterLegalLinks(webActions.getPage(), footer.selectors);
   });
 
-  test('@medium @footer @apps @cinesa should display mobile app download links', async ({ page, footer }) => {
-    await assertFooterAppLinks(page, footer.selectors);
+  test('should display mobile app download links', 
+    { tag: ['@medium', '@footer', '@apps', '@cinesa'] },
+    async ({ webActions, footer }) => {
+    await assertFooterAppLinks(webActions.getPage(), footer.selectors);
   });
 });
